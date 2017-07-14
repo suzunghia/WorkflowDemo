d08b99de526864e825508eac91ee6b6d99d78880
ngo_trong_nghia <nghiant6554@co-well.com.vn>
2017-07-14 10:29:50 +0700

    init

---
+++ src/main/java/com/workflow/demo/WorkflowDemoApplication.java
@@ -0,0 +1,12 @@
+package com.workflow.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowDemoApplication.class, args);
+	}
+}
