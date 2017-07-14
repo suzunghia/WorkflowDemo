74bf444f134604b253ddb55593e6093cd6c99d72
ngo_trong_nghia <nghiant6554@co-well.com.vn>
2017-07-14 12:31:04 +0700

    igniteUI - angular

--- src/main/java/com/workflow/demo/controllers/MainController.java
+++ src/main/java/com/workflow/demo/controllers/MainController.java
@@ -9,6 +9,6 @@ public class MainController {
 	@GetMapping("/")
     public String test(Model model) {
         model.addAttribute("msg", "Work-flow demo");
-        return "test";
+        return "angular";
     }
 }
